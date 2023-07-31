17d43da6b3778360d8e6ba1f7f506d0a7e2b1ca7
yoo16 <yohei.yoshikawa@gmail.com>
2023-07-31 17:25:58 +0900

    Personの処理を追加

--- Person.java
+++ Person.java
@@ -1,3 +1,14 @@
 public class Person {
     
+    public String name;
+    public String gender;
+    
+    public Person(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public void speack() {
+        System.out.println("私の名前は" + this.name + "です。");
+    }
 }
---
+++ PersonApp.java
@@ -0,0 +1,8 @@
+public class PersonApp {
+    
+    public static void main(String[] args) {
+        Person person1 = new Person("Yoshikawa", "male");        
+        person1.speack();
+    }
+
+}
